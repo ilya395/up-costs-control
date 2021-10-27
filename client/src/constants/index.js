8846827557f0205edeacfee6be4a18b527611b6f
AXAPTA\iefimenko <AXAPTA\iefimenko>
2021-10-27 23:55:17 +0300

    add new styles 2

---
+++ client/src/constants/index.js
@@ -0,0 +1,14 @@
+export const buttonColors = new Set([
+  "#FF9B5F",
+  "#FFE55F",
+  "#C2FF5F",
+  "#6CFF5F",
+  "#5FFFB2",
+  "#5FE2FF",
+  "#5F82FF",
+  "#C25FFF",
+  "#F25FFF",
+  "#FF5FC9",
+  "#FF5F5F",
+  "#C15340"
+]);
--- client/src/forms/ExpenseItemForm/view/ExpenseItemForm.view.jsx
+++ client/src/forms/ExpenseItemForm/view/ExpenseItemForm.view.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { buttonColors } from "../../../constants";
 
 export const ExpenseItemFormView = () => {
+  const getButtons = () => {
+    const array = [];
+    buttonColors.forEach(item => {
+      const arg = (
+        <div className="form-field__list-item" key={item}>
+          <input type="radio" id={item} name="color" value={item} className="form-field__radio-input" />
+          <label htmlFor={item} className="form-field__radio-label" style={{backgroundColor: item}}></label>
+        </div>
+      );
+      array.push(arg);
+    });
+    return array;
+  }
   return (
     <form className="simple-form">
       <div className="simple-form__form-field simple-form__form-field_row">
@@ -8,10 +22,14 @@ export const ExpenseItemFormView = () => {
         {/* <input type="text" className="simple-form__input simple-text_main" id="name" name="name" /> */}
         <span className="simple-text_main form-field__simple-text">Образование. На себя. Образование. На себя.</span>
       </div>
-      <div className="simple-form__form-field simple-form__form-field_row">
-        <label className="simple-text_main" htmlFor="name">Название:</label>
-        <div className="form-field__list">
-
+      <div className="simple-form__form-field simple-form__form-field_column">
+        <div className="simple-text_main">Цвет:</div>
+        <div className="form-field__list-wrapper">
+          <div className="form-field__list">
+            {
+              getButtons()
+            }
+          </div>
         </div>
       </div>
     </form>
