511f239e622567d4d690bad7c12605c257573a3a
AXAPTA\iefimenko <AXAPTA\iefimenko>
2021-10-27 18:05:05 +0300

    add new styles 1

--- client/src/forms/ExpenseItemForm/view/ExpenseItemForm.view.jsx
+++ client/src/forms/ExpenseItemForm/view/ExpenseItemForm.view.jsx
@@ -8,6 +8,12 @@ export const ExpenseItemFormView = () => {
         {/* <input type="text" className="simple-form__input simple-text_main" id="name" name="name" /> */}
         <span className="simple-text_main form-field__simple-text">Образование. На себя. Образование. На себя.</span>
       </div>
+      <div className="simple-form__form-field simple-form__form-field_row">
+        <label className="simple-text_main" htmlFor="name">Название:</label>
+        <div className="form-field__list">
+
+        </div>
+      </div>
     </form>
     );
 }
